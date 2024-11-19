a0f04fda1738be2bbe799308c67a40cf358b0e27
Jam <sagajamesedson@gmail.com>
2024-11-19 14:52:19 +0800

    Added read functionality to display tasks

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
+<?php
+include 'db.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $task = $_POST['task'];
+    $sql = "INSERT INTO tasks (task) VALUES ('$task')";
+    if ($conn->query($sql) === TRUE) {
+        header("Location: index.php");
+        exit;
+    } else {
+        echo "Error: " . $conn->error;
+    }
+}
+
+$sql = "SELECT * FROM tasks ORDER BY created_at DESC";
+$result = $conn->query($sql);
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -16,6 +33,18 @@
             <button type="submit" class="btn">Add Task</button>
         </form>
 
+        <ul>
+            <?php while ($row = $result->fetch_assoc()): ?>
+                <li>
+                    <span><?php echo htmlspecialchars($row['task']); ?></span>
+                    <div class="task-actions">
+                        <a href="edit_task.php?id=<?php echo $row['id']; ?>" class="btn edit">Edit</a>
+                        <a href="delete_task.php?id=<?php echo $row['id']; ?>" class="btn delete">Delete</a>
+                    </div>
+                </li>
+            <?php endwhile; ?>
+        </ul>
     </div>
 </body>
 </html>
+
